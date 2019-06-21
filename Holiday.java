b063de156a84a669dbb3ce8831d6b3b912c3be4d
elikemkomla <ebuame@gmail.com>
2019-06-21 16:46:36 +0100

    number 1

---
+++ Holiday.java
@@ -0,0 +1,36 @@
+public class Holiday {
+	private String name;
+	private int day;
+	private String month;
+
+ public Holiday(String a, int b, String c){
+ name = a;
+ day = b;
+ month = c;
+ }
+ public static inSameMonth() {
+ public String getMonth(){
+ return month;
+ public boolean equals(inSameMonth other){
+ if(other.getMonth().equals(this.getMonth)){
+ if(other.getMonth()!=this.getMonth()){
+ return false;
+ }else{
+ return true;
+ }
+ }
+ }
+ }
+
+ }
+ public static double avgDate(Holiday[] freeday){
+ int sum = 0;
+ for(int i = 0; i < holiday.length; ++) {
+     sum = sum + holiday(i).day;
+     }
+     return((double)sum)/holiday.length;
+     }
+     Holiday freeday = new Holiday("Independence day", 4, "July");
+ }
+ 
+ 
